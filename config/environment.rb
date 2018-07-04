0539b1fee8e10270365946c24f394a610d1b10c1
Alexandre Castoldi <alexandre.castoldi@gmail.com>
2018-07-04 01:17:43 +0000

    Change MAILGUN password'

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ ActionMailer::Base.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :password       => '7907ac131d9ada7cd5e1a4c9f1179701-770f03c4-de7274c5',
   :domain         => 'radiant-hollows-65088.herokuapp.com',
   :authentication => :plain,
 }
